70a8eee63eaf309ba014d0831d32d17a712ac1a8
Gabriel de Dompablo <124939163+wannabetri@users.noreply.github.com>
2023-08-04 23:59:06 +0200

    Add files via upload

---
+++ my site/ads/views.py
@@ -0,0 +1,24 @@
+from ads.models import Ad
+from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+
+
+class AdListView(OwnerListView):
+    model = Ad
+
+
+class AdDetailView(OwnerDetailView):
+    model = Ad
+
+
+class AdCreateView(OwnerCreateView):
+    model = Ad
+    fields = ["title", "price", "text"]
+
+
+class AdUpdateView(OwnerUpdateView):
+    model = Ad
+    fields = ["title", "price", "text"]
+
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
